02e53176ec871b5bc782dca7a4f8f4edbd66e23a
sumesharavind <126322997+sumesharavind@users.noreply.github.com>
2023-03-05 14:27:11 +0530

    Add files via upload

---
+++ indexs.js
@@ -0,0 +1,18 @@
+ const hours= document.querySelector('.hrs');
+ const minutes= document.querySelector('.mins');
+ const seconds= document.querySelector('.secs');
+
+ setInterval(runClock,1000);
+ function runClock(){
+const time=new Date();
+const secs=time.getSeconds()/60;
+const mins=(secs+time.getMinutes())/60;
+const hrs=(mins+time.getHours())/12;
+ 
+ hours.style.setProperty("--rotation",hrs*360);
+ minutes.style.setProperty("--rotation",mins*360);
+ seconds.style.setProperty("--rotation",secs*360);
+
+
+ }
+ runClock();
